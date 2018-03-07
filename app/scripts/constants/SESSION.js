6c95f2f8b726161e9989637338b09311d976cb28
Michael <mpkelley08@gmail.com>
2018-03-07 15:05:06 -0500

    added break functionality

---
+++ app/scripts/constants/SESSION.js
@@ -0,0 +1,6 @@
+
+(function(){
+  angular
+    .module('pomTimer')
+    .constant('SESSION', {WORK:1500, BREAK_SHORT:300})
+})();
--- app/scripts/controllers/HomeCtrl.js
+++ app/scripts/controllers/HomeCtrl.js
@@ -1,23 +1,43 @@
 (function() {
-    function HomeCtrl($interval) {
+    function HomeCtrl($interval, SESSION) {
       this.time = 1500;
-      this.break = true;
+      this.onBreak = false;
       this.workButtonName = "Start A Work Session";
+      this.breakButtonName = "Start A Break Session"
       var counting = false;
       var begin;
 
 
-      this.runTimer = (total) => {
+      this.workTimer = () => {
         if (counting === false) {
           counting = true;
-          this.time = total;
+          this.time = SESSION.WORK;
           begin = $interval(this.countdown, 1000);
+          this.breakButtonName = "Start A Break Session";
           this.workButtonName = "Reset Only - No breaks yet!";
        }
        else {
          counting = false;
-         this.time = total;
+         this.time = SESSION.WORK;
          this.workButtonName = "Start A Work Session";
+         $interval.cancel(begin);
+       }
+
+      }
+
+
+      this.breakTimer = () => {
+        if (counting === false) {
+          counting = true;
+          this.time = SESSION.BREAK_SHORT;
+          begin = $interval(this.countdown, 1000);
+          this.workButtonName = "Start A Work Session";
+          this.breakButtonName = "Reset Only - No Pausing!";
+       }
+       else {
+         counting = false;
+         this.time = SESSION.BREAK_SHORT;
+         this.breakButtonName = "Start A Break Session";
          $interval.cancel(begin);
        }
 
@@ -26,6 +46,11 @@
 
       this.countdown = () => {
         this.time--
+        if(this.time <= 0) {
+          $interval.cancel(begin);
+          counting = false;
+          this.onBreak ? this.onBreak = false : this.onBreak = true;
+        }
       }
 
 
@@ -37,5 +62,5 @@
 
     angular
         .module('pomTimer')
-        .controller('HomeCtrl', ['$interval', HomeCtrl]);
+        .controller('HomeCtrl', ['$interval','SESSION', HomeCtrl]);
 })();
